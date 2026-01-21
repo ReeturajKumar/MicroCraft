1ed7cabf93870bc1730a10785fcb377f87e80f24
ReeturajKumar <reeturajvats587@gmail.com>
2026-01-21 17:58:27 +0530

    build fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"; // Import Navigate
+import { BrowserRouter, Route, Routes} from "react-router-dom"; // Import Navigate
 import Home from "./pages/Home";
-import CrmManagement from "./pages/CrmManagement";
-import AIAutomation from "./pages/AIAutomation";
+// import CrmManagement from "./pages/CrmManagement";
+// import AIAutomation from "./pages/AIAutomation";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import TopStrip from "./components/TopStrip";
-import DigitalCommerce from "./pages/DigitalCommerce";
-import Analytics from "./pages/Analytics";
-import CaseStudies from "./pages/CaseStudies";
-import Integration from "./pages/Integration";
-import Sectors from "./pages/Sectors";
-import Consultation from "./pages/Consultation";
-import Resources from "./pages/Resources";
+// import DigitalCommerce from "./pages/DigitalCommerce";
+// import Analytics from "./pages/Analytics";
+// import CaseStudies from "./pages/CaseStudies";
+// import Integration from "./pages/Integration";
+// import Sectors from "./pages/Sectors";
+// import Consultation from "./pages/Consultation";
+// import Resources from "./pages/Resources";
 import ScrollToTop from "./components/ScrollToTop";
 import LoadingIndicator from "./components/LoadingIndicator";
 
